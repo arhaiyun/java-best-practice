0af2f7db16c3e7d341df5a2c59c484d076bfdbfa
arhaiyun <vrhaiyun@gmail.com>
2020-04-25 13:41:39 +0800

    java best practices

---
+++ src/main/java/com/exodus/arhaiyun/fundamentals/multithread/ThreadPoolExecutorDemo.java
@@ -0,0 +1,70 @@
+package com.exodus.arhaiyun.fundamentals.multithread;
+
+import java.util.concurrent.*;
+
+public class ThreadPoolExecutorDemo {
+
+    private static final RejectedExecutionHandler defaultHandler = new RejectedExecutionHandler() {
+        @Override
+        public void rejectedExecution(Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+
+        }
+    };
+    private final int corePoolSize;
+    private final int maximumPoolSize;
+    private final BlockingQueue<Runnable> workQueue;
+    private final long keepAliveTime;
+    private final ThreadFactory threadFactory;
+    private final RejectedExecutionHandler handler;
+
+    public ThreadPoolExecutorDemo(int corePoolSize,
+                                  int maximumPoolSize,
+                                  long keepAliveTime,
+                                  TimeUnit unit,
+                                  BlockingQueue<Runnable> workQueue) {
+        this(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
+                Executors.defaultThreadFactory(), defaultHandler);
+    }
+
+    public ThreadPoolExecutorDemo(int corePoolSize,
+                                  int maximumPoolSize,
+                                  long keepAliveTime,
+                                  TimeUnit unit,
+                                  BlockingQueue<Runnable> workQueue,
+                                  ThreadFactory threadFactory) {
+        this(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
+                threadFactory, defaultHandler);
+    }
+
+    public ThreadPoolExecutorDemo(int corePoolSize,
+                                  int maximumPoolSize,
+                                  long keepAliveTime,
+                                  TimeUnit unit,
+                                  BlockingQueue<Runnable> workQueue,
+                                  RejectedExecutionHandler handler) {
+        this(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
+                Executors.defaultThreadFactory(), handler);
+    }
+
+    public ThreadPoolExecutorDemo(int corePoolSize,
+                                  int maximumPoolSize,
+                                  long keepAliveTime,
+                                  TimeUnit unit,
+                                  BlockingQueue<Runnable> workQueue,
+                                  ThreadFactory threadFactory,
+                                  RejectedExecutionHandler handler) {
+        if (corePoolSize < 0 ||
+                maximumPoolSize <= 0 ||
+                maximumPoolSize < corePoolSize ||
+                keepAliveTime < 0)
+            throw new IllegalArgumentException();
+        if (workQueue == null || threadFactory == null || handler == null)
+            throw new NullPointerException();
+        this.corePoolSize = corePoolSize;
+        this.maximumPoolSize = maximumPoolSize;
+        this.workQueue = workQueue;
+        this.keepAliveTime = unit.toNanos(keepAliveTime);
+        this.threadFactory = threadFactory;
+        this.handler = handler;
+    }
+}
