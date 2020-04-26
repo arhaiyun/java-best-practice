8fb97e9e8115e193926966f8ce9960d728dd9660
arhaiyun <vrhaiyun@gmail.com>
2020-04-26 18:59:42 +0800

    SearchInSortedArray

---
+++ src/main/java/com/exodus/leetcode/bytedance/SearchInSortedArray.java
@@ -0,0 +1,71 @@
+package com.exodus.leetcode.bytedance;
+
+/**
+ * @author arhaiyun
+ * @version 1.0
+ * @date 2020/4/26 18:44
+ * <p>
+ * <p>
+ * 33. Search in Rotated Sorted Array
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * <p>
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * <p>
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * <p>
+ * You may assume no duplicate exists in the array.
+ * <p>
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * Example 2:
+ * <p>
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ */
+public class SearchInSortedArray {
+
+    public static int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+//        int target = 3;
+//        System.out.println(search(nums, target));
+//        -1
+        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(nums, target));
+    }
+}
