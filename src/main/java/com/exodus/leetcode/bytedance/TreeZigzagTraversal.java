2e1cb42411128e959411f6aac958492c1dd68d38
arhaiyun <vrhaiyun@gmail.com>
2020-04-27 21:30:43 +0800

    TreeZigzagTraversal

---
+++ src/main/java/com/exodus/leetcode/bytedance/TreeZigzagTraversal.java
@@ -0,0 +1,65 @@
+package com.exodus.leetcode.bytedance;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author arhaiyun
+ * @version 1.0
+ * @date 2020/4/27 18:58
+ * <p>
+ * 103. Binary Tree Zigzag Level Order Traversal
+ * Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+ * <p>
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 3
+ * / \
+ * 9  20
+ *   /  \
+ *  15   7
+ * return its zigzag level order traversal as:
+ * [
+ * [3],
+ * [20,9],
+ * [15,7]
+ * ]
+ */
+public class TreeZigzagTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        // 使用队列作为数据结构实现广度优先遍历
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        // 记录是否反转
+        boolean isReverse = false;
+
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> curLevel = new LinkedList<>();
+            // 每次都取出一层的所有数据
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                if (!isReverse) {
+                    curLevel.add(node.val);
+                } else {
+                    curLevel.addFirst(node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            isReverse = !isReverse;
+            result.add(curLevel);
+        }
+        return result;
+    }
+}
