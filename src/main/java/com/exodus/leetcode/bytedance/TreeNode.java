87cee1c2b4bd94b7802ab427f3c039361ab0acb2
arhaiyun <vrhaiyun@gmail.com>
2020-04-27 18:56:57 +0800

    LowestCommonAncestor

---
+++ src/main/java/com/exodus/leetcode/bytedance/TreeNode.java
@@ -0,0 +1,20 @@
+package com.exodus.leetcode.bytedance;
+
+/**
+ * @author arhaiyun
+ * @version 1.0
+ * @date 2020/4/27 18:30
+ */
+
+//Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
