8f4113059f42c3cce3989775f431981e7e3a419f
arhaiyun <vrhaiyun@gmail.com>
2020-05-02 12:32:49 +0800

    UTF_8Validation & SecondHighestSalary

---
+++ src/main/java/com/exodus/leetcode/bytedance/UTF_8Validation.java
@@ -0,0 +1,101 @@
+package com.exodus.leetcode.bytedance;
+
+/**
+ * @author arhaiyun
+ * @version 1.0
+ * @date 2020/5/2 12:00
+ * <p>
+ * <p>
+ * A character in UTF8 can be from 1 to 4 bytes long, subjected to the following rules:
+ * <p>
+ * For 1-byte character, the first bit is a 0, followed by its unicode code.
+ * For n-bytes character, the first n-bits are all one's, the n+1 bit is 0, followed by n-1 bytes with most significant 2 bits being 10.
+ * This is how the UTF-8 encoding would work:
+ * <p>
+ * Char. number range  |        UTF-8 octet sequence
+ * (hexadecimal)    |              (binary)
+ * --------------------+---------------------------------------------
+ * 0000 0000-0000 007F | 0xxxxxxx
+ * 0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+ * 0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+ * 0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+ * Given an array of integers representing the data, return whether it is a valid utf-8 encoding.
+ * <p>
+ * Note:
+ * The input is an array of integers. Only the least significant 8 bits of each integer is used to store the data. This means each integer represents only 1 byte of data.
+ * <p>
+ * Example 1:
+ * <p>
+ * data = [197, 130, 1], which represents the octet sequence: 11000101 10000010 00000001.
+ * <p>
+ * Return true.
+ * It is a valid utf-8 encoding for a 2-bytes character followed by a 1-byte character.
+ * Example 2:
+ * <p>
+ * data = [235, 140, 4], which represented the octet sequence: 11101011 10001100 00000100.
+ * <p>
+ * Return false.
+ * The first 3 bits are all one's and the 4th bit is 0 means it is a 3-bytes character.
+ * The next byte is a continuation byte which starts with 10 and that's correct.
+ * But the second continuation byte does not start with 10, so it is invalid.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/utf-8-validation
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class UTF_8Validation {
+    public static boolean validUtf8(int[] data) {
+
+        // Number of bytes in the current UTF-8 character
+        int numberOfBytesToProcess = 0;
+
+        // For each integer in the data array.
+        for (int datum : data) {
+            // Get the binary representation. We only need the least significant 8 bits
+            // for any given number.
+            String binRep = Integer.toBinaryString(datum);
+            binRep = binRep.length() >= 8
+                    ? binRep.substring(binRep.length() - 8)
+                    : "00000000".substring(binRep.length() % 8) + binRep;
+            // If this is the case then we are to start processing a new UTF-8 character.
+            if (numberOfBytesToProcess == 0) {
+                // Get the number of 1s in the beginning of the string.
+                for (int j = 0; j < binRep.length(); j++) {
+                    if (binRep.charAt(j) == '0') {
+                        break;
+                    }
+                    numberOfBytesToProcess += 1;
+                }
+                // 1 byte characters
+                if (numberOfBytesToProcess == 0) {
+                    continue;
+                }
+                // Invalid scenarios according to the rules of the problem.
+                if (numberOfBytesToProcess > 4 || numberOfBytesToProcess == 1) {
+                    return false;
+                }
+            } else {
+                // Else, we are processing integers which represent bytes which are a part of
+                // a UTF-8 character. So, they must adhere to the pattern `10xxxxxx`.
+                if (!(binRep.charAt(0) == '1' && binRep.charAt(1) == '0')) {
+                    return false;
+                }
+            }
+            // We reduce the number of bytes to process by 1 after each integer.
+            numberOfBytesToProcess -= 1;
+        }
+        // This is for the case where we might not have the complete data for
+        // a particular UTF-8 character.
+        return numberOfBytesToProcess == 0;
+    }
+
+    public static void main(String[] args) {
+        String binRep = Integer.toBinaryString(256);
+        binRep = binRep.length() >= 8
+                ? binRep.substring(binRep.length() - 8)
+                : "00000000".substring(binRep.length() % 8) + binRep;
+        System.out.println(Integer.toBinaryString(256));
+        System.out.println(binRep);
+        System.out.println(validUtf8(new int[]{192, 128, 192, 128}));
+    }
+}
