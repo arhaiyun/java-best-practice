b4dade5b8b686f6078ba16e24caf2f2b69c24a7d
arhaiyun <vrhaiyun@gmail.com>
2020-05-02 16:46:26 +0800

    MinStack

---
+++ src/main/java/com/exodus/leetcode/bytedance/MinStack.java
@@ -0,0 +1,81 @@
+package com.exodus.leetcode.bytedance;
+
+import java.util.Stack;
+
+/**
+ * @author arhaiyun
+ * @version 1.0
+ * @date 2020/5/2 16:18
+ * <p>
+ * <p>
+ * 155. Min Stack
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * <p>
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * <p>
+ * Output
+ * [null,null,null,null,-3,null,0,-2]
+ * <p>
+ * Explanation
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin(); // return -3
+ * minStack.pop();
+ * minStack.top();    // return 0
+ * minStack.getMin(); // return -2
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * Methods pop, top and getMin operations will always be called on non-empty stacks.
+ */
+public class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new Stack<>();
+        this.minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty()) {
+            minStack.push(x);
+        } else {
+            int top = minStack.peek();
+            if (x <= top) {
+                minStack.push(x);
+            }
+        }
+    }
+
+    public void pop() {
+        int pop = stack.pop();
+        if (pop == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
