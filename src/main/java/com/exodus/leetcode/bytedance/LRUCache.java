9d41e4618fd90283361a115de197b29aae158af9
arhaiyun <vrhaiyun@gmail.com>
2020-05-02 16:15:44 +0800

    LRUCache

---
+++ src/main/java/com/exodus/leetcode/bytedance/LRUCache.java
@@ -0,0 +1,143 @@
+package com.exodus.leetcode.bytedance;
+
+import java.util.HashMap;
+
+/**
+ * @author arhaiyun
+ * @version 1.0
+ * @date 2020/5/2 15:47
+ * <p>
+ * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+ * <p>
+ * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ * put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+ * <p>
+ * The cache is initialized with a positive capacity.
+ * <p>
+ * Follow up:
+ * Could you do both operations in O(1) time complexity?
+ * <p>
+ * Example:
+ * <p>
+ * LRUCache cache = new LRUCache( 2 /* capacity  );
+ * <p>
+ * <p>
+ * cache.put(1,1);
+ * cache.put(2,2);
+ * cache.get(1);       // returns 1
+ * cache.put(3,3);    // evicts key 2
+ * cache.get(2);       // returns -1 (not found)
+ * cache.put(4,4);    // evicts key 1
+ * cache.get(1);       // returns -1 (not found)
+ * cache.get(3);       // returns 3
+ * cache.get(4);       // returns 4
+ */
+
+
+class Node {
+    public int key, val;
+    public Node next, prev;
+
+    public Node(int k, int v) {
+        this.key = k;
+        this.val = v;
+    }
+}
+
+class DoubleList {
+    private Node head, tail; // 头尾虚节点
+    private int size; // 链表元素数
+
+    public DoubleList() {
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+        size = 0;
+    }
+
+    // 在链表头部添加节点 x
+    public void addFirst(Node x) {
+        x.next = head.next;
+        x.prev = head;
+        head.next.prev = x;
+        head.next = x;
+        size++;
+    }
+
+    // 删除链表中的 x 节点（x 一定存在）
+    public void remove(Node x) {
+        x.prev.next = x.next;
+        x.next.prev = x.prev;
+        size--;
+    }
+
+    // 删除链表中最后一个节点，并返回该节点
+    public Node removeLast() {
+        if (tail.prev == head)
+            return null;
+        Node last = tail.prev;
+        remove(last);
+        return last;
+    }
+
+    // 返回链表长度
+    public int size() {
+        return size;
+    }
+}
+
+public class LRUCache {
+    // key -> Node(key, val)
+    private HashMap<Integer, Node> map;
+    // Node(k1, v1) <-> Node(k2, v2)...
+    private DoubleList cache;
+    // 最大容量
+    private int cap;
+
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+        map = new HashMap<>();
+        cache = new DoubleList();
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+        int val = map.get(key).val;
+        // 利用 put 方法把该数据提前
+        put(key, val);
+        return val;
+    }
+
+    public void put(int key, int val) {
+        // 先把新节点 x 做出来
+        Node x = new Node(key, val);
+
+        if (map.containsKey(key)) {
+            // 删除旧的节点，新的插到头部
+            cache.remove(map.get(key));
+        } else if (cap == cache.size()) {
+            // 删除链表最后一个数据
+            Node last = cache.removeLast();
+            map.remove(last.key);
+        }
+        cache.addFirst(x);
+        map.put(key, x);
+    }
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2 /* capacity */);
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.get(1);       // returns 1
+        cache.put(3, 3);    // evicts key 2
+        cache.get(2);       // returns -1 (not found)
+        cache.put(4, 4);    // evicts key 1
+        cache.get(1);       // returns -1 (not found)
+        cache.get(3);       // returns 3
+        System.out.println(cache.get(4));
+
+    }
+}
