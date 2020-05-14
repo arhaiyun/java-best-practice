1dbae49ccb08fafc1f169dc469f4dcc071849e2e
arhaiyun <vrhaiyun@gmail.com>
2020-05-14 22:35:04 +0800

    Single number

---
+++ src/main/java/com/exodus/leetcode/SingleNumber.java
@@ -0,0 +1,40 @@
+package com.exodus.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author arhaiyun
+ * @version 1.0
+ * @date 2020/5/14 22:24
+ */
+public class SingleNumber {
+    public static int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+
+    public int singleNumber2(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer i : nums) {
+            Integer count = map.get(i);
+            count = count == null ? 1 : ++count;
+            map.put(i, count);
+        }
+        for (Integer i : map.keySet()) {
+            Integer count = map.get(i);
+            if (count == 1) {
+                return i;
+            }
+        }
+        return -1; // can't find it.
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{4, 1, 2, 1, 2};
+        System.out.println(singleNumber(nums));
+    }
+}
